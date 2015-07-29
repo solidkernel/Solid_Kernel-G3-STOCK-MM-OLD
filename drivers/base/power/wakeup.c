66de1c7a8bf3b868f71e0ccefbf9f20b93a31190
Loller79 <loller790@gmail.com>
2015-07-29 13:21:49 +0200

    PM: Introduce suspend state PM_SUSPEND_FREEZE

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -380,6 +380,12 @@ EXPORT_SYMBOL_GPL(device_set_wakeup_enable);
  */
 static void wakeup_source_activate(struct wakeup_source *ws)
 {
+	/*
+	 * active wakeup source should bring the system
+	 * out of PM_SUSPEND_FREEZE state
+	 */
+	freeze_wake();
+
 	unsigned int cec;
 #if defined(CONFIG_MACH_MSM8974_B1_KR) || defined(CONFIG_MACH_MSM8974_B1W)
 	extern int boost_freq;
