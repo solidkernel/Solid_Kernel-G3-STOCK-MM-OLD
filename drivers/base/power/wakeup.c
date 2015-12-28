d774da620f0718c27e98700983f9381b43a409e2
Loller79 <loller790@gmail.com>
2015-12-28 16:55:19 +0100

    Linaro 4.8 & Fixes

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -380,13 +380,12 @@ EXPORT_SYMBOL_GPL(device_set_wakeup_enable);
  */
 static void wakeup_source_activate(struct wakeup_source *ws)
 {
+	unsigned int cec;
 	/*
 	 * active wakeup source should bring the system
 	 * out of PM_SUSPEND_FREEZE state
 	 */
 	freeze_wake();
-
-	unsigned int cec;
 
 	ws->active = true;
 	ws->active_count++;
